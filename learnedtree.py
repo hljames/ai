27b4e60376c5796310a1a9af01194f07cf0f5051
hljames <haileyjames@college.harvard.edu>
2018-05-10 16:34:43 +0200

    updates

--- learnedtree.py
+++ learnedtree.py
@@ -3,14 +3,17 @@ import pandas as pd
 import itertools
 import steinertree as ST
 
-# metrics.adjusted_mutual_info_score(labels_true, labels_pred)
-
 gv = pd.read_excel('generatedvalues.xlsx', 'test', index_col=None, na_values=['NA'])
 
-vars = gv.columns
+# the variable from the network (InternationalIntervention, SouthChinaFisheryCollapse, etc)
+vars = list(gv.columns)
+# create a list of all the possible pairs of variables
 varpairs = list(itertools.combinations(vars, 2))
+edges = []
+# get the mutual information from each pair, and add it as the edge weight of the network
+for x_1,x_2 in varpairs:
+    mi_weight = metrics.adjusted_mutual_info_score(gv[x_1], gv[x_2])
+    edges.append([vars.index(x_1),vars.index(x_2),-1.0*float(mi_weight)])
 
-# for x_1,x_2 in varpairs:
-    # get the mi of the pair, add the pairs and the edge weight to the list of edge_subgraph
-
-learnedgraph = ST.Graph(len(vars),len(varpairs),edges);
+# run the steinertree algorithm on the network, and return the result 
+learnedgraph = ST.Graph(len(vars),len(vars),edges).steinerTree();
